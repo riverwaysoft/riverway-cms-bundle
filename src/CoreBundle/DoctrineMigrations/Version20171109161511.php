fd70ee0f1a1149edc5bcd3a775d75bac962853d9
Sorokin Victor <st-agrail@yandex.ru>
2017-11-09 19:16:40 +0300

    add migration

---
+++ src/CoreBundle/DoctrineMigrations/Version20171109161511.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Riverway\Cms\CoreBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171109161511 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE slide ADD margin_top INT NOT NULL, DROP vertical_align');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE slide ADD vertical_align VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, DROP margin_top');
+    }
+}
