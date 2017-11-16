f4ed0d0359e122160fc66b5f345c5fe0c939c41e
Ann Tarkovskaya <ann@riverway.it>
2017-11-16 16:57:06 +0300

    Fix

--- src/CoreBundle/Form/WidgetType.php
+++ src/CoreBundle/Form/WidgetType.php
@@ -40,7 +40,7 @@ class WidgetType extends AbstractType
                 }
             });
 
-            $builder->addEventListener(FormEvents::POST_SUBMIT,
+            $builder->addEventListener(FormEvents::PRE_SUBMIT,
                 function (FormEvent $formEvent) use ($widget) {
                     /** @var Widget $entity */
                     $entity = $formEvent->getForm()->getData();
