8b873f0c6b79b5f3602bdf8ee50e5854b8afba61
kate-kate <koalaester@gmail.com>
2017-11-21 18:20:14 +0300

    new templates

--- src/CoreBundle/Enum/TemplateEnum.php
+++ src/CoreBundle/Enum/TemplateEnum.php
@@ -11,6 +11,8 @@ use MyCLabs\Enum\Enum;
  * @method static TemplateEnum CUSTOM_FULL()
  * @method static TemplateEnum CUSTOM_ONE_THIRD_RS()
  * @method static TemplateEnum CUSTOM_ONE_THIRD_LS()
+ * @method static TemplateEnum TESTIMONIALS()
+ * @method static TemplateEnum SERVICE()
  */
 class TemplateEnum extends Enum
 {
@@ -19,4 +21,6 @@ class TemplateEnum extends Enum
     const CUSTOM_FULL = 'custom/full.html.twig';
     const CUSTOM_ONE_THIRD_RS = 'custom/one_third_rs.html.twig';
     const CUSTOM_ONE_THIRD_LS = 'custom/one_third_ls.html.twig';
+    const TESTIMONIALS = 'testimonials.html.twig';
+    const SERVICE = 'service.html.twig';
 }
